3a467a9ef9522fb532f83dd8ebdb24cefe339961
alexfromyandex <151350420+alexfromyandex@users.noreply.github.com>
2026-01-19 01:14:00 +0500

    Update start_project.sh

--- bin/start_project.sh
+++ bin/start_project.sh
@@ -3,7 +3,7 @@
 echo "ЗАПУСК ПРОЕКТА"
 cp $DIR_TESTS/data/.env.example $GITHUB_WORKSPACE/backend/.env
 cd $DIR_TESTS/data
-docker compose up -d
+docker compose up
 
 cd $GITHUB_WORKSPACE/backend/
 npm cache clean --force
